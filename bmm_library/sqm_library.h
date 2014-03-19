f88fab7dec91313e128bbbcd2eecbc463b5aa1ef
misop <michal.piovarci@gmail.com>
2014-03-19 20:44:35 +0100

    Lib dependencies addition

--- bmm_library/sqm_library.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-#include "SQMSkeletonNode.h"
-
-void SQMLoadFromFileAndExportToFile(std::string input, std::string output, bool CPUSkinning = false);
-void SQMLoadFromFileAndExportToVectors(std::string input, std::vector<float> &points, std::vector<int> &indices, bool CPUSkinning = false);
-
-void SQMLoadFromSkeletonAndExportToFile(SQMSkeletonNode *skeleton, std::string output, bool CPUSkinning = false);
-void SQMLoadFromSkeletonAndExportToVectors(SQMSkeletonNode *skeleton, std::vector<float> &points, std::vector<int> &indices, bool CPUSkinning = false);
